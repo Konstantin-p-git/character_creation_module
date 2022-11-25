9f3e0b079d9b9fe97120af310b092b814146a572
Konstantin <konstantin.p.git@gmail.com>
2022-11-25 04:09:44 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from random import randint
 
 
+# Новый импорт.
+# Из модуля start_game_banner, который расположен в папке graphic_arts,
+# импортируем функцию run_screensaver().
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """Функция attack рассчитывает урон для классов."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон, равный {5 + randint(3, 5)}')
     if char_class == 'mage':
@@ -11,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция defence рассчитывает защиту для классов."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -20,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция special рассчитывает специальное умение для классов."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -32,6 +41,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция start_training запускает тренировку."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -56,6 +66,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция choice_char_class позволяет выбрать класс персонажа."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -78,7 +89,9 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    """Основная функция запускающая нужную последовательность функций."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -90,4 +103,4 @@ def main():
     print(start_training(char_name, char_class))
 
 
-main()
+
